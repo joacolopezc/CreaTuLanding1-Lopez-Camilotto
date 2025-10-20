e89ffc90c75a82c3ce0e4ecb8285f8f074fbea62
Joaquin Lopez <joaquincamilotto@gmail.com>
2025-10-20 14:46:37 +0200

    v0.1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import Button from './components/Button'
+
+function App() {
+  return (
+    <div>
+      <Button label='click me' />
+      <Button label='login' />
+      <Button label='logout' />
+      <Button label='report' />
+    </div>
+  )
+}
+
+export default App
